5ab25eb923898315c12c15001b8ee438d1607bc0
gio <hosting@404.ge>
2015-03-02 00:19:40 +0400

    welcome page

--- application/views/parts/footer.php
+++ application/views/parts/footer.php
@@ -1,4 +1,11 @@
-
+<div class="container content_container">
+	<div class="row">
+		<div class="col-lg-12 footer">
+			<hr />
+			<h6>Support: +995 599 62 35 55</h6>
+		</div>
+	</div>	
+</div>
 <script src="dist/js/scripts.js" charset="utf-8"></script>
 </body>
 </html>
--- application/views/view_admin_home.php
+++ application/views/view_admin_home.php
@@ -2,13 +2,46 @@
 @include("application/views/parts/header.php");
 @include("application/views/parts/top.php");
 
-// echo "<pre>";
-// print_r($all_userdata);
-// echo "</pre>";
 if(empty($all_userdata["username"])){
 	@include("application/views/parts/loginform.php");
 }else{
-	echo "congrats you are logged in !";
+	?>
+		<div class="container content_container">
+			<div class="row">
+				<div class="col-lg-12">
+					<h3>Welcome to Website: "Gio" CMS</h3><br />
+					
+					<p><b>Website Name:</b> Gio website</p>
+					<p><b>Website Address:</b> <a href="http://redis.404.ge" target="_blank">www.redis.404.ge</a></p>
+					<p><b>User:</b> Gio (administrator)</p>
+					<p><b>CMS version:</b> 1.0.0</p>
+					<p><b>Your IP Address:</b> 176.73.234.42</p>
+				</div>
+				<div class="col-lg-4">
+					<h4>Site managment</h4>
+					
+					<p><b>Top menu</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+					<p><b>Main menu</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+					<p><b>Aditional menu</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+					<p><b>Menu managment</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+				</div>
+				<div class="col-lg-4">
+					<h4>Users</h4>					
+					<p><b>User list</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+					<p><b>User rights</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+					<p><b>Site user</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+				</div>
+				<div class="col-lg-4">
+					<h4>Settings</h4>
+					<p><b>Change password</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+					<p><b>Site settings</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+					<p><b>Backups</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+					<p><b>logs</b> <a href="#" class="editlink"><i class="fa fa-pencil-square-o"></i></a></p>
+				</div>
+			</div>	
+
+		</div>
+	<?php
 }
 
 @include("application/views/parts/footer.php");
